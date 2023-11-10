f71d908667fefd8d0a978905f027df8f517a1bb3
any <tangjiacheng.h5route@bytedance.com>
2023-11-10 10:48:41 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "preserve",
+  arrowParens: "always",
+};
