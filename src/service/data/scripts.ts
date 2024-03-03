b10bbf3883a6ecfa96d438a6a3c342ed620637d2
hunhou001 <55705194+hunhou001@users.noreply.github.com>
2024-03-03 16:30:04 +0800

    fix: 脚本中文标题

--- src/service/data/scripts.ts
+++ src/service/data/scripts.ts
@@ -20,51 +20,53 @@ const useGameScriptsData = () => {
   const functionsTree = useMemo(() => {
     if (!functionsData.data || !functionsComment.data) return void 0;
     const toScriptTreeNode = (data: RecordTree<string>, comment: CommentNode, path: string[]): ScriptTreeNode => {
-      if (comment._type === 'textarea') {
+      if (comment._type === "textarea") {
         return {
-          title: path.at(-1) ?? '',
+          title: (path.at(-1) ?? "") + " / " + comment._data,
           desc: comment._data,
-          path: path.join('/'),
+          path: path.join("/"),
         };
-      } else if (comment._type === 'object') {
+      } else if (comment._type === "object") {
         const children = Object.entries(comment._data).map(([k, v]) => {
           return toScriptTreeNode(data[k] as RecordTree<string>, v as CommentNode, path.concat([k]));
         });
         return {
-          title: path.at(-1) ?? '',
-          path: path.join('/'),
+          title: path.at(-1) ?? "",
+          path: path.join("/"),
           children,
-        }
+        };
       } else {
         throw Error();
       }
-    }
-    return toScriptTreeNode(functionsData.data, functionsComment.data, ['functions']);
+    };
+    return toScriptTreeNode(functionsData.data, functionsComment.data, ["functions"]);
   }, [functionsData, functionsComment]);
 
   const readScript = (path: string) => {
-    const [scope, ...innerPath] = path.split('/');
-    if (scope === 'functions') {
+    const [scope, ...innerPath] = path.split("/");
+    if (scope === "functions") {
       if (!functionsData.data) throw Error();
       return get(functionsData.data, innerPath);
     }
-  }
+  };
 
   const writeScript = (path: string, data: string) => {
-    const [scope, ...innerPath] = path.split('/');
-    if (scope === 'functions') {
+    const [scope, ...innerPath] = path.split("/");
+    if (scope === "functions") {
       if (!functionsData.data) throw Error();
-      functionsData.write(produce(functionsData.data, (draft) => {
-        set(draft, innerPath, data);
-      }));
+      functionsData.write(
+        produce(functionsData.data, (draft) => {
+          set(draft, innerPath, data);
+        }),
+      );
     }
-  }
+  };
 
   return {
     functionsTree,
     readScript,
     writeScript,
-  }
-}
+  };
+};
 
 export const GameScriptsDataModel = createModel(useGameScriptsData);
